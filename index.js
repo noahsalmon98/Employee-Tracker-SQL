e1f99e1fdef942f5efb7336278a4cff71cc03ca7
Noah Salmon <noahsalmon98@gmail.com>
2023-09-13 12:53:46 -0400

    added function menuOptions to index.js

--- index.js
+++ index.js
@@ -9,3 +9,60 @@ const db = mysql.createConnection({
     password: '',
     database: 'company_db'
 }, console.log('connected to company_db database'));
+
+//TO DO
+  //view all departments
+  //view all roles
+  //view all employees
+  //add a department
+  //add a role
+  //add an employee
+  //exit
+
+
+
+function menuOptions() {
+    const choices =
+    {
+        type: 'list',
+        message: 'What would you like to do?',
+        name: 'answer',
+        choices: ['View all departments',
+            'View all roles',
+            'View all employees',
+            'Add a department',
+            'Add a role',
+            'Add an employee',
+            'Quit']
+    }
+
+    prompt(choices)
+        .then((response) => {
+            switch (response.answer) {
+                case 'View all departments':
+                    viewDepartment();
+                    break;
+                case 'View all roles':
+                    viewRoles();
+                    break;
+                case 'View all employees':
+                    viewEmployees();
+                    break;
+                case 'Add a department':
+                    addDepartment();
+                    break;
+                case 'Add a role':
+                    addRole();
+                    break;
+                case 'Add an employee':
+                    addEmployee();
+                    break;
+                case 'Quit':
+                    console.log('Done!')
+                    break;
+            }
+        });
+}
+
+//TO DO
+  //functions for each case
